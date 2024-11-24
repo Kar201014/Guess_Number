e5fb72f93aec91a6d828aa34c8dd339f6bd837dd
Kar201014 <karenkarapetian2@gmail.com>
2024-11-24 13:56:23 +0400

    Add files via upload

---
+++ guess_number.py
@@ -0,0 +1,16 @@
+# import random
+
+# def generate_random_number():
+#     return random.randint(1, 100)
+
+# def guess_number(target, guess): 
+#     if guess < target:
+#         return "Too low! Try again."
+#     elif guess > target:
+#         return "Too high! Try again."
+#     else:
+#         return "Congratulations! You guessed the correct number."
+
+# # # # Example usage:
+# target_number = generate_random_number()
+# print(guess_number(target_number, 50))
